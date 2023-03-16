4743a8e054ed4afb44ff1f3ffaa8e739613cbad2
Diego Imbert <diegoimbert@pm.me>
2023-03-16 09:37:25 +0100

    parse use statements in file

--- compilers/hs-elyonc/src/Elyon/Parser/File.hs
+++ compilers/hs-elyonc/src/Elyon/Parser/File.hs
@@ -11,13 +11,15 @@ import Elyon.Parser (Parser, lxs)
 import Elyon.Parser.Terms (PTerm, PTPattern, termP, termPatternP)
 import Elyon.Parser.Assignments (PAssignment, assignmentP)
 import Elyon.Parser.Traits (PTraitDecl, traitP, implP) 
+import Elyon.Parser.UseStatements (PUseStatement, useStmtP)
 import Text.Parsec.Indent (topLevel)
-import Text.Parsec (many, char, (<|>))
+import Text.Parsec (many, char, (<|>), string)
 
 data PFileContent =
     PFA_Assignment (PAssignment PTPattern PTerm)
   | PFA_TraitDecl PTraitDecl
   | PFA_ImplDecl PTraitDecl
+  | PFA_UseStmt PUseStatement
   deriving (Show, Eq)
 
 fileP :: Parser [PFileContent]
@@ -27,5 +29,6 @@ fileContentP :: Parser PFileContent
 fileContentP =
       fmap PFA_TraitDecl traitP
   <|> fmap PFA_ImplDecl implP
+  <|> fmap PFA_UseStmt (string "use " *> useStmtP)
   <|> fmap PFA_Assignment 
       (assignmentP termPatternP termP (char '='))
